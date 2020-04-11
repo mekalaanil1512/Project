1dc82f45f9786825587a84eba3495e89fb6a8bd1
Cloud User <centos@ip-172-31-13-55.ap-south-1.compute.internal>
2020-04-11 15:28:56 +0000

    app done 1

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -1,41 +1,72 @@
 package com.ravi.cal.RaviCalculator;
 
+
 import java.util.Scanner;
 
-public class Calculator  {
-	
-	private long first;
-	private long second;
-	
-	public long getFirst() {
-		return first;
-	}
+public class Calculator {
 
-	public void setFirst(long first) {
-		this.first = first;
-	}
+     private long first;
+     private long second;
 
-	public long getSecond() {
-		return second;
-	}
+     private long getFirst() {
+       return first;
+      }
 
-	public void setSecond(long second) {
-		this.second = second;
-	}
+    public void setFirst(long first) {
+       this.first = first;
+      }
 
-	public long addFucn(long first, long second){
-		
-		return first+second;
-	}
-	
-	public long subFucn(long first, long second){
-		
-		return second-first;
-	}
-	
-	public long mulFucn(long first, long second){
-		
-		return first*second;
-	}
+    private long getSecond() {
+       return second;
+     }
+
+    public void setSecond(long second{
+        this.second = second;
+    }
+
+    public long addFunc(long first, long second) {
+       return first+second;
+    }
+
+    public long subFunc(long first,long second)
+    {
+       return second-first;
+    }
+
+
+    public long mulFunc(long first, long second)
+    {
+        return first*second;
+    }
+
+   public static void main(String[] args)
+   {
+   Calculator cc = new Calculator();
+   Scanner sc = new Scanner(System.in);
+
+   System.out.println("Enter the first numeric value:");
+   long first = sc.nextlong();
+   cc.setFirst(first);
+
+   System.out.println("Enter the second numeric value:");
+   long second = sc.nextLong();
+   cc.setSecond(second);
+
+   System.out.println("The Sum of two numbers is:" +cc.addFunc(first,second));
+   System.out.println("The difference of two numbers is:"+cc.subFunc(first,second));
+   System.out.println("The Multiplication of two numbers is:"+cc.mulFunc(first,second));
+
+  }
+
 }
+   
+
+
+
+
+
+
+
+
+
 
