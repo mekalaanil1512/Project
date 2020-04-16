aea06f99387e6a2fe88d6ffe442d06d9ccbe1eaa
mekalaanil1512 <60133011+mekalaanil1512@users.noreply.github.com>
2020-04-17 01:11:21 +0530

    bug

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -36,7 +36,7 @@ public class Calculator {
 
     public long mulFunc(long first, long second)
     {
-        return first*second;
+        return first/second;
     }
 
    public static void main(String[] args)
